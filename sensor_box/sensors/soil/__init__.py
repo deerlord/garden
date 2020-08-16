eb953bf5b73b648828f167ad8ff41b6d7ef910a5
Trent Dierking <deerxlord@gmail.com>
2020-08-16 17:01:30 -0500

    updates

--- sensor_box/sensors/soil/__init__.py
+++ sensor_box/sensors/soil/__init__.py
@@ -1,10 +1,26 @@
+from dataclasses import dataclass
 from .. import Sensor
+import json
+import os.path
 
 
+@dataclass
 class Moisture(Sensor):
+    voltage: float
+
+    def __post_init__(self):
+        cwd = os.path.dirname(os.path.realpath(__file__))
+        with open(f'{cwd}/moisture_voltages.json') as f:
+            config = json.load(f)
+        self.__value_map = config[str(self.voltage)]
+
+    def __clamp_value(self, value):
+        return max(0, min(self.__value_map['100'], value))
+
     def _transform(self, value):
+        value = (value * float(self.__value_map['100']))
         return value
 
-
-moisture = Moisture(getter=lambda: 0.0)
+print('making Moisture')
+moisture = Moisture(getter=lambda: 0.0, voltage=3.3)
 print(moisture.value)
