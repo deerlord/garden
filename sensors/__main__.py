bfa52c4f18cd686281e98c457930af26906d3238
Trent Dierking <deerxlord@gmail.com>
2020-08-17 22:38:51 -0500

    sensors phase 1

--- sensors/__main__.py
+++ sensors/__main__.py
@@ -1,6 +1,7 @@
 import influxdb
 import socket
 from time import sleep
+from gpiozero import LED
 from . import Multiplexer
 
 
@@ -12,12 +13,15 @@ client = influxdb.InfluxDBClient(
     database='gardens'
 )
 multiplexer = Multiplexer(voltage=3.3)
+devices = LED(26)
 while True:
+    devices.on()
     data_wrapper = [{
         'measurement': garden_boxes,
         'tags': [socket.gethostname()],
         'time': time,
         'fields': {index: value for index, value in enumerate((multiplexer))}
     }]
+    devices.off()
     client.write_points(data_wrapper)
     sleep(3600)
